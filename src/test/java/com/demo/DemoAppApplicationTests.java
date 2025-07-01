99fb40446f101c5574e3b474653a18c52ac525c4
Mohamed Esmael <mohamed.2714104@gmail.com>
2025-07-01 18:20:24 +0300

    Update DemoAppApplicationTests.java

--- src/test/java/com/demo/DemoAppApplicationTests.java
+++ src/test/java/com/demo/DemoAppApplicationTests.java
@@ -1,13 +1,23 @@
 package com.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoAppApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
+        
+        
+        assertThat(context.containsBeanDefinition("demoAppApplication")).isTrue();
     }
 }
-
