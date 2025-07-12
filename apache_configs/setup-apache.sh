d52d65032b4d478d9b6a0befaba1c0eacb8d525e
Mohamed Esmael <mohamed.2714104@gmail.com>
2025-07-12 22:36:53 +0300

    Update setup-apache.sh

--- apache_configs/setup-apache.sh
+++ apache_configs/setup-apache.sh
@@ -2,15 +2,14 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y apache2 libapache2-mod-proxy-html
+sudo apt-get install -y apache2
 
-sudo systemctl enable apache2 --now
-
-sudo a2enmod proxy proxy_http ssl
+# Enable Apache modules
+sudo a2enmod proxy proxy_http proxy_html ssl
 
 CONFIG_BASE=https://raw.githubusercontent.com/mohamedesmael10/comprehensive-production-ci-cd-pipeline/git-actions-pipeline/apache_configs
 
-sudo curl -fsSL $CONFIG_BASE/jenkins.conf -o /etc/apache2/sites-available/jenkins.conf
+sudo curl -fsSL $CONFIG_BASE/jenkins.conf   -o /etc/apache2/sites-available/jenkins.conf
 sudo curl -fsSL $CONFIG_BASE/sonarqube.conf -o /etc/apache2/sites-available/sonarqube.conf
 
 sudo a2ensite jenkins.conf
